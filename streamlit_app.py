75eaa99db1ae76375468911921710952cd22043a
jaeminson153 <jaeminson153@gmail.com>
2025-08-04 17:22:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.title("나의 첫 스트림릿 앱")
-st.write("Hello, Streamlit!")
+st.title("제목입니다")
+st.header("헤더입니다")
+st.subheader("서브헤더입니다")
+st.write("일반 텍스트입니다")
+st.markdown("**굵은 글씨**와 *기울임*")
